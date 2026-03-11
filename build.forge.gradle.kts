5bc0d4528d8fa12a1aa0b37f08daa5a263e1f86c
IAFEnvoy <tanksheng545@126.com>
2026-03-11 09:27:09 +0800

    add dependencies

--- build.forge.gradle.kts
+++ build.forge.gradle.kts
@@ -139,11 +139,13 @@ publishMods {
         projectId = property("publish.modrinth") as String
         accessToken = env.MODRINTH_API_KEY.orNull()
         minecraftVersions.addAll(supportedMinecraftVersions)
+        requires("server-i18n-api")
     }
 
     curseforge {
         projectId = property("publish.curseforge") as String
         accessToken = env.CURSEFORGE_API_KEY.orNull()
         minecraftVersions.addAll(supportedMinecraftVersions)
+        requires("server-i18n-api")
     }
 }
